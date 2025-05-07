d341071ba21f329212a4a80d59f6596a3d95b326
Sergii Gnatiuk <gnatiuk.s@ajax.systems>
2025-05-07 21:55:11 +0300

    test badges

--- src/generate-badges.js
+++ src/generate-badges.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const core = require('@actions/core');
 const gradientBadge = require('gradient-badge');
+const core = require('@actions/core');
 
 const badgesOutputDir = 'badges/';
 fs.mkdirSync(badgesOutputDir, {recursive: true});

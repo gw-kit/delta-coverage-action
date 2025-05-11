f66a364815f6e379aab44d71d88998878a3a0fee
Sergii Gnatiuk <surpsg2012@gmail.com>
2025-05-11 18:01:14 +0300

    Generate coverage badges

---
+++ src/read-summaries.js
@@ -0,0 +1,21 @@
+module.exports = async (ctx) => {
+    const fs = require('fs');
+
+    const fullCoverageFilter = file => file.includes('full-coverage-');
+    const deltaCoverageFilter = file => !fullCoverageFilter(file);
+
+    const allSummariesFile = ctx.isFullCoverageMode ? 'full-cov-summaries.json' : 'delta-cov-summaries.json';
+
+    const chosenFilter = ctx.isFullCoverageMode ? fullCoverageFilter : deltaCoverageFilter;
+
+    const files = fs.readdirSync(ctx.baseSummariesPath);
+    const summaryFiles = files.filter(file => file.includes('-summary.json')).filter(chosenFilter);
+    console.log(`Reading summaries from ${ctx.baseSummariesPath}: ${JSON.stringify(summaryFiles, null, 2)}`);
+
+    const summaries = summaryFiles.map(file =>
+        JSON.parse(fs.readFileSync(`${ctx.baseSummariesPath}/${file}`, 'utf8'))
+    );
+    fs.writeFileSync(allSummariesFile, JSON.stringify(summaries));
+
+    return allSummariesFile;
+};
